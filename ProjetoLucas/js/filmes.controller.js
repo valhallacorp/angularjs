6f5e4fb2cec7a9698af155e2f4def97b8b2d0f28
lucaslimoni <39009011+lucaslimoni@users.noreply.github.com>
2018-05-05 12:18:59 -0300

    Add files via upload
    Sistema para estudo de angularjs

---
+++ ProjetoLucas/js/filmes.controller.js
@@ -0,0 +1,23 @@
+angular.module("Filmes").controller("FilmesController", function($scope){
+	$scope.titulo = "Filmes que já assisti!";
+
+	$scope.filmes = [];
+
+	$scope.novoFilme = {};
+
+	$scope.removerFilme = function(id) {
+		angular.forEach($scope.filmes, function(filme, i){
+			if(filme.id == id){
+				$scope.filmes.splice(i, 1);
+			}
+		});
+	}
+
+	$scope.adicionarFilme = function() {
+		var filme = angular.copy($scope.novoFilme);
+		filme.id = Date.now();
+		$scope.filmes.push(filme);
+
+		$scope.novoFilme = {};
+	}
+});
